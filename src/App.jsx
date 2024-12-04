9546183d438c4870c7cbcb0ea11dd6db16ddb5a1
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-04 17:20:39 +0100

    import components-in-app.jsx-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
 import { GlobalContextProvider, } from "./context/GlobalContext"
-
+import MediaCard from "./components/MediaCard"
+import AppHeader from "./components/AppHeader"
+import MovieList from "./components/MovieList"
+import ShowList from "./components/ShowList"
 
 function App() {
 
