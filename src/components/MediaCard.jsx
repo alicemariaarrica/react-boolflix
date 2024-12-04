b9f57ed4c1d97e7f9d4fd76b9af7a5bc9c3f5902
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-04 17:13:51 +0100

    mediacard-update

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,44 +1,37 @@
-import ReactCountryFlag from "react-country-flag"
-import Rating from "./Rating"
 
-export default function MediaCard({ title, original, vote, lang, overview, poster }) {
+import { useGlobalContext } from "../contexts/GlobalContext";
+import Rating from "./Rating";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFlag } from "@fortawesome/free-solid-svg-icons";
 
+export default function MediaCard({ title, original, vote, overview, poster, lang }) {
+    const imageURL = `https://image.tmdb.org/t/p/w342/${poster}`;
+
+
+    const getFlagURL = (language) => {
+        const flagBaseURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/";
+        const langToFlag = {
+            "it": "4/42/Flag_of_Italy.svg/120px-Flag_of_Italy.svg.png",
+            "en": "1/1f/Flag_of_the_United_States.svg/120px-Flag_of_the_United_States.svg.png",
+            "fr": "c/c3/Flag_of_France.svg/120px-Flag_of_France.svg.png",
+
+        };
+
+        return langToFlag[language] || "default-flag.png";
+    };
 
     return (
-        <div className="col" style={{ borderBottom: '1px solid black', marginBottom: '1rem' }}>
-
-
-            <div className="media_card">
-
-                {
-                    poster ? <img className="img-fluid" src={`https://image.tmdb.org/t/p/w342/${poster}`} alt="" /> : <img className="img-fluid" src={`https://placehold.co/342x513?text=${title}`} alt="" />
-                }
-
-
-                <div className="media_card_details">
-
-                    <h6>{original}</h6>
-                    <h3>{title}</h3>
-
-                    <Rating vote={vote} />
-
-
-                    <div>Laanguage:  {lang}</div>
-
-                    <ReactCountryFlag className="emojiFlag" countryCode={lang === 'en' ? 'us' : lang} svg style={{
-                        width: '2em',
-                        height: '2em',
-                    }} />
-
-
-                    <p>
-                        {overview}
-                    </p>
+        <div className="media-card">
+            <img src={imageURL} alt={title} className="poster-img" />
+            <div className="media-card-details">
+                <h3>{title}</h3>
+                <p>{original}</p>
+                <div className="flag">
+                    <img src={getFlagURL(lang)} alt={`Flag of ${lang}`} />
                 </div>
-
+                <Rating vote={vote} />
+                <p>{overview}</p>
             </div>
-
-
         </div>
-    )
+    );
 }
